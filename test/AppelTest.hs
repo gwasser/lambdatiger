bbabdccf9895eb15db577e190e4f57b5b82b02bb
Garret Wassermann <gwasser@gmail.com>
2017-08-10 13:52:26 -0400

    Adapt unit tests to read tests from file, and add Appel's tests folder

--- test/AppelTest.hs
+++ test/AppelTest.hs
@@ -31,41 +31,28 @@ import Tiger.Syntactic.AST (Program(..), Exp(..), Var(..), Decl(..), Type(..), O
                              
 -- these tests are based on the ones written by Appel in sample ML code,
 -- and distributed via the textbook website
-appelTests = testGroup "Appel's original tests for Tiger" [testAppel1, testAppel2, testAppel3, testAppel4, testAppel5, testAppel12]
+appelTests = testGroup "Appel's original tests for Tiger" [testAppel1, testAppel2, testAppel3, testAppel4, testAppel5, testAppel6, testAppel7, testAppel8, testAppel12]
 
 testAppel1 =
-    testCase "parses test1" $ assertEqual [] (Program $ LetExp [TypeDecl "arrtype" (ArrayType "int"), VarDecl "arr1" True (Just "arrtype") (ArrayExp "arrtype" (IntExp 10) (IntExp 0))] (VarExp $ SimpleVar "arr1")) (happyTokenParse $ scanner "let type arrtype = array of int var arr1:arrtype := arrtype [10] of 0 in arr1 end")
+    testCase "parses test1 (read from file)" $ (readFile "test/appeltestcases/test1.tig" >>= (assertEqual [] (Program $ LetExp [TypeDecl "arrtype" (ArrayType "int"), VarDecl "arr1" True (Just "arrtype") (ArrayExp "arrtype" (IntExp 10) (IntExp 0))] (VarExp $ SimpleVar "arr1"))) . happyTokenParse . scanner)
 testAppel2 =
-    testCase "parses test2" $ assertEqual [] (Program $ LetExp [TypeDecl "myint" (NameType "int"), TypeDecl "arrtype" (ArrayType "myint"), VarDecl "arr1" True (Just "arrtype") (ArrayExp "arrtype" (IntExp 10) (IntExp 0))] (VarExp $ SimpleVar "arr1")) (happyTokenParse $ scanner "let type myint = int type  arrtype = array of myint var arr1:arrtype := arrtype [10] of 0 in arr1 end")
+    testCase "parses test2 (read from file)" $ (readFile "test/appeltestcases/test2.tig" >>= (assertEqual [] (Program $ LetExp [TypeDecl "myint" (NameType "int"), TypeDecl "arrtype" (ArrayType "myint"), VarDecl "arr1" True (Just "arrtype") (ArrayExp "arrtype" (IntExp 10) (IntExp 0))] (VarExp $ SimpleVar "arr1"))) . happyTokenParse . scanner)
 testAppel3 =
-    testCase "parses test3" $ assertEqual [] (Program $ LetExp [TypeDecl "rectype" (RecordType [Field "name" True "string", Field "age" True "int"]), VarDecl "rec1" True (Just "rectype") (RecordExp [("age", IntExp 1000), ("name", StrExp "Nobody")] ("rectype"))] (SeqExp [AssignExp (FieldVar (SimpleVar "rec1") "name") (StrExp "Somebody"), VarExp $ SimpleVar "rec1"])) (happyTokenParse $ scanner "let \
-	\type  rectype = {name:string, age:int} \
-	\var rec1:rectype := rectype {name=\"Nobody\", age=1000} \
-\in \
-	\rec1.name := \"Somebody\"; \
-	\rec1 \
-\end")
+    testCase "parses test3 (read from file)" $ (readFile "test/appeltestcases/test3.tig" >>= (assertEqual [] (Program $ LetExp [TypeDecl "rectype" (RecordType [Field "name" True "string", Field "age" True "int"]), VarDecl "rec1" True (Just "rectype") (RecordExp [("age", IntExp 1000), ("name", StrExp "Nobody")] ("rectype"))] (SeqExp [AssignExp (FieldVar (SimpleVar "rec1") "name") (StrExp "Somebody"), VarExp $ SimpleVar "rec1"]))) . happyTokenParse . scanner)
 testAppel4 =
-    testCase "parses test4" $ assertEqual [] (Program (LetExp {decls = [FunDecls [FunDecl {fundeclname = "nfactor", params = [Field {fieldname = "n", escape = True, typ = "int"}], result = Just "int", body = IfExp {iftest = OpExp {left = VarExp (SimpleVar "n"), oper = Equal, right = IntExp 0}, thenexp = IntExp 1, elseexp = Just (OpExp {left = VarExp (SimpleVar "n"), oper = Mul, right = CallExp {func = "nfactor", args = [OpExp {left = VarExp (SimpleVar "n"), oper = Sub, right = IntExp 1}]}})}}]], lbody = CallExp {func = "nfactor", args = [IntExp 10]}})) (happyTokenParse $ scanner "let \
-\/* calculate n! */ \
-\function nfactor(n: int): int = \
-	\if  n = 0 \
-        \then 1 \
-        \else n * nfactor(n-1) \
-\in \
-	\nfactor(10) \
-\end")
+    testCase "parses test4 (read from file)" $ (readFile "test/appeltestcases/test4.tig" >>= (assertEqual [] (Program (LetExp {decls = [FunDecls [FunDecl {fundeclname = "nfactor", params = [Field {fieldname = "n", escape = True, typ = "int"}], result = Just "int", body = IfExp {iftest = OpExp {left = VarExp (SimpleVar "n"), oper = Equal, right = IntExp 0}, thenexp = IntExp 1, elseexp = Just (OpExp {left = VarExp (SimpleVar "n"), oper = Mul, right = CallExp {func = "nfactor", args = [OpExp {left = VarExp (SimpleVar "n"), oper = Sub, right = IntExp 1}]}})}}]], lbody = CallExp {func = "nfactor", args = [IntExp 10]}}))) . happyTokenParse . scanner)
 testAppel5 =
-    testCase "parses test5" $ assertEqual [] (Program (LetExp {decls = [TypeDecl {tname = "intlist", ttyp = RecordType [Field {fieldname = "hd", escape = True, typ = "int"},Field {fieldname = "tl", escape = True, typ = "intlist"}]}, TypeDecl {tname = "tree", ttyp = RecordType [Field {fieldname = "key", escape = True, typ = "int"},Field {fieldname = "children", escape = True, typ = "treelist"}]},TypeDecl {tname = "treelist", ttyp = RecordType [Field {fieldname = "hd", escape = True, typ = "tree"},Field {fieldname = "tl", escape = True, typ = "treelist"}]},VarDecl {vname = "lis", vescape = True, vtyp = Just "intlist", vinit = RecordExp {fields = [("tl",NilExp),("hd",IntExp 0)], rtyp = "intlist"}}], lbody = VarExp (SimpleVar "lis")})) (happyTokenParse $ scanner "let \
-\/* define a list */ \
-\type intlist = {hd: int, tl: intlist} \ 
-\/* define a tree */ \
-\type tree ={key: int, children: treelist} \
-\type treelist = {hd: tree, tl: treelist} \
-\var lis:intlist := intlist { hd=0, tl= nil } \ 
-\in \
-	\lis \
-\end")
+    testCase "parses test5 (read from file)" $ (readFile "test/appeltestcases/test5.tig" >>= (assertEqual [] (Program (LetExp {decls = [TypeDecl {tname = "intlist", ttyp = RecordType [Field {fieldname = "hd", escape = True, typ = "int"},Field {fieldname = "tl", escape = True, typ = "intlist"}]}, TypeDecl {tname = "tree", ttyp = RecordType [Field {fieldname = "key", escape = True, typ = "int"},Field {fieldname = "children", escape = True, typ = "treelist"}]},TypeDecl {tname = "treelist", ttyp = RecordType [Field {fieldname = "hd", escape = True, typ = "tree"},Field {fieldname = "tl", escape = True, typ = "treelist"}]},VarDecl {vname = "lis", vescape = True, vtyp = Just "intlist", vinit = RecordExp {fields = [("tl",NilExp),("hd",IntExp 0)], rtyp = "intlist"}}], lbody = VarExp (SimpleVar "lis")}))) . happyTokenParse . scanner)
+testAppel6 =
+    testCase "parses test6 (read from file)" $ (readFile "test/appeltestcases/test6.tig" >>= (assertEqual [] (Program $ (LetExp {decls = [FunDecls [FunDecl {fundeclname = "do_nothing1", params = [Field {fieldname = "a", escape = True, typ = "int"},Field {fieldname = "b", escape = True, typ = "string"}], result = Nothing, body = CallExp {func = "do_nothing2", args = [OpExp {left = VarExp (SimpleVar "a"), oper = Add, right = IntExp 1}]}},FunDecl {fundeclname = "do_nothing2", params = [Field {fieldname = "d", escape = True, typ = "int"}], result = Nothing, body = CallExp {func = "do_nothing1", args = [VarExp (SimpleVar "d"),StrExp "str"]}}]], lbody = CallExp {func = "do_nothing1", args = [IntExp 0,StrExp "str2"]}}))) . happyTokenParse . scanner)
+testAppel7 =
+    testCase "parses test7 (read from file)" $ (readFile "test/appeltestcases/test7.tig" >>= (assertEqual [] (Program $ (LetExp {decls = [FunDecls [FunDecl {fundeclname = "do_nothing1", params = [Field {fieldname = "a", escape = True, typ = "int"},Field {fieldname = "b", escape = True, typ = "string"}], result = Just "int", body = SeqExp [CallExp {func = "do_nothing2", args = [OpExp {left = VarExp (SimpleVar "a"), oper = Add, right = IntExp 1}]},IntExp 0]},FunDecl {fundeclname = "do_nothing2", params = [Field {fieldname = "d", escape = True, typ = "int"}], result = Just "string", body = SeqExp [CallExp {func = "do_nothing1", args = [VarExp (SimpleVar "d"),StrExp "str"]},StrExp " "]}]], lbody = CallExp {func = "do_nothing1", args = [IntExp 0,StrExp "str2"]}}))) . happyTokenParse . scanner)
+testAppel8 =
+    testCase "parses test8 (read from file)" $ (readFile "test/appeltestcases/test8.tig" >>= (assertEqual [] (Program $ IfExp {iftest = OpExp {left = IntExp 10, oper = GreaterThan, right = IntExp 20}, thenexp = IntExp 30, elseexp = Just $ IntExp 40})) . happyTokenParse . scanner)
 testAppel12 =
-  testCase "parses test12" $ assertEqual [] (Program $ LetExp [VarDecl "a" True Nothing (IntExp 0)] (ForExp "i" True (IntExp 0) (IntExp 100) (SeqExp [AssignExp (SimpleVar "a") (OpExp (VarExp $ SimpleVar "a") Add (IntExp 1)), SeqExp []]))) (happyTokenParse $ scanner "/* valid for and let */ let var a:= 0 in for i:=0 to 100 do (a:=a+1;()) end")
-                                                                                                                                                                                                                                                        
+    testCase "parses test12 (read from file)" $ (readFile "test/appeltestcases/test12.tig" >>= (assertEqual [] (Program $ LetExp [VarDecl "a" True Nothing (IntExp 0)] (ForExp "i" True (IntExp 0) (IntExp 100) (SeqExp [AssignExp (SimpleVar "a") (OpExp (VarExp $ SimpleVar "a") Add (IntExp 1)), SeqExp []])))) . happyTokenParse . scanner)
+
+    
+-- testAppel6 =
+    --testCase "parses test6 (read from file)" $ (readFile "test/appeltestcases/test6.tig" >>= (assertEqual [] (Program $ NilExp)) . happyTokenParse . scanner)
+    
